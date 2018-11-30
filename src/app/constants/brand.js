31252c247a03bf517f771a10ae5386f56f383b6f
vking34 <lzzvkingzzl@gmail.com>
2018-11-30 08:51:53 +0700

    init

---
+++ src/app/constants/brand.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'AVD transport',
+  desc: 'This admin page made by VKing34',
+  prefix: 'VKING34',
+  footerText: 'Dont insult clients',
+  logoText: 'AVD transport',
+  needLogin: false
+};
---
+++ src/app/constants/menu.js
@@ -0,0 +1,50 @@
+module.exports = [
+  {
+    key: 'dashboard',
+    name: 'Dashboard',
+    icon: 'dashboard',
+    link: '/admin'
+  },
+  {
+    key: 'layouts',
+    name: 'Route',
+    icon: 'view_column',
+    link: '/admin/tables/crud-table'
+  },
+  {
+    key: 'themes',
+    name: 'Transporter',
+    icon: 'palette',
+    link: '/admin/themes'
+  },
+  {
+    key: 'tables',
+    name: 'Employee',
+    icon: 'grid_on',
+    link: '/admin/tables/crud-table'
+  },
+  {
+    key: 'forms',
+    name: 'Clients',
+    icon: 'border_color',
+    link: '/admin/tables/crud-table'
+  },
+  {
+    key: 'ui',
+    name: 'Transport Schedule',
+    icon: 'flag',
+    link: '/admin/tables/crud-table'
+  },
+  {
+    key: 'charts',
+    name: 'Work Schedule',
+    icon: 'insert_chart',
+    link: '/admin/tables/crud-table'
+  },
+  {
+    key: 'themes',
+    name: 'Themes Setting',
+    icon: 'palette',
+    link: '/admin/settings/themes'
+  },
+];
